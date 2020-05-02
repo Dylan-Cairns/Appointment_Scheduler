29bce069515dc88abdb2a3c0e09907ba9bac00bc
dylan <dcairns@wgu.edu>
2020-05-02 20:30:45 +0800

    added comments

--- src/Utils/TimeFunctions.java
+++ src/Utils/TimeFunctions.java
@@ -36,7 +36,9 @@ public class TimeFunctions {
         ObservableList<LocalTime> availableTimeSlots = FXCollections.observableArrayList();
         ObservableList<LocalDateTime> nineToFiveSchedule = generate9to5Schedule(selectedDate);
         ObservableList<Appointment> existingAppts = DataStorage.getAllAppointments();
-
+        //get a list of potential timeslots from the generate9to5Schedule fn.
+        //check if each timeslot falls at the same time as the start of an existing appt, or during
+        // and existing appt. If so, remove it from our results.
         for(LocalDateTime timeSlot: nineToFiveSchedule) {
             for(Appointment appt: existingAppts) {
                 if(timeSlot == appt.getStartTime()){
@@ -47,6 +49,7 @@ public class TimeFunctions {
                 }
             }
         }
+        //strip the date from each timeslot to prepare it for use in the combobox.
         for(LocalDateTime ldt: nineToFiveSchedule) {
             availableTimeSlots.add(LocalTime.from(ldt));
         }
@@ -54,6 +57,8 @@ public class TimeFunctions {
     }
 
     private static ObservableList<LocalDateTime> generate9to5Schedule(LocalDate selectedDate) {
+        //create a list of LocalDateTime objects in 15 minute intervals starting from 09:00
+        // and with the last one at 16:45
         ObservableList<LocalDateTime> nineToFiveSchedule = FXCollections.observableArrayList();
         ObservableList<String> hourPeriods = FXCollections.observableArrayList();
         hourPeriods.addAll("09", "10", "11", "12", "13", "14", "15", "16");
@@ -68,6 +73,10 @@ public class TimeFunctions {
                 nineToFiveSchedule.add(timeBlock);
             }
         }
+        //remove the last item in the list, which is the 16:45 timeslot.
+        //this time is too late to schedule an appointment.
+        nineToFiveSchedule.remove(nineToFiveSchedule.size() -1);
+        //return generated list
         return nineToFiveSchedule;
     }
 }
